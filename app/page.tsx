0391f2b85176d1f485c9dfa99dcf759696dd080e
jayachandra27 <jayachandra.s.reddy@outlook.com>
2023-08-07 09:34:04 +0530

    added title and desc

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import next_logo from '../public/next.svg'
 export default function Home() {
   return (
         <div>
-          <h2 className='text-4xl text-center mt-72'>databracket.ai is cooking...</h2>
-          <p className='text-xl text-center'>Please comeback soon as we are wrapping up our go-live</p>
+          <h2 className='text-6xl text-center mt-72 md:mt-72 lg:mt-72'>databracket.ai is cooking...</h2>
+          <p className='text-2xl text-center'>Please comeback soon as we are wrapping up our go-live</p>
         </div>
       )
 }
