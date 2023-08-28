0ef39e98069bfdfd110a9f5b0f660671696c5173
jayachandra27 <jayachandra.s.reddy@outlook.com>
2023-08-28 15:47:08 +0530

    page restructuring and social media logos added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import Image from 'next/image'
-import vercel from '../public/vercel.svg'
-import next_logo from '../public/next.svg'
+import React from 'react';
+import Header from '@/pages/header';
+import Hero from '@/pages/hero';
 
 export default function Home() {
   return (
-        <div>
-          <h2 className='text-6xl text-center mt-72 md:mt-72 lg:mt-72'>databracket.ai is cooking...</h2>
-          <p className='text-2xl text-center'>Please comeback soon as we are wrapping up our go-live</p>
+        <div className="bg-popular-primary ">
+        <div className="container mx-auto text-center">
+          <Header/>
+          <Hero />
+        </div>
         </div>
       )
 }
---
+++ pages/hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Hero() {
+    return (
+        <div className='bg-ternary-color'>
+        <div>
+            <h1 className="text-4xl font-bold text-gray-800 ">Databracket.ai</h1>
+            <p className='font-bold text-xl text-popular-light mb-4'>Uniting Data and AI Solutions.</p>
+        </div>
+        <div className='flex justify-center items-center'>
+        <h3 className="text-center">
+            Databracket is committed at furnishing individuals with the knowledge, resources, and strategies needed to  <br />
+            excel at and leverage the potential of <b>Data Engineering, DevOps and MLOps</b><br />
+            to transform innovative <b>AI</b> concepts/ideas into commercial products/services.
+        </h3>
+        </div>
+        </div>
+        )
+  }
