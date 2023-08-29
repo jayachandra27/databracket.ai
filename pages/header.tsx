fac04c2972afb62a4db410be80e967ecbbef0454
jayachandra27 <jayachandra.s.reddy@outlook.com>
2023-08-29 11:57:52 +0530

    corrected services text

--- pages/header.tsx
+++ pages/header.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
+
 export default function Header() {
     return(
         <header className="flex items-center justify-between p-4 shadow-md">
