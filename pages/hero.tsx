8cbaade97eef9ff9866f4fa2932b99328169921b
jayachandra27 <jayachandra.s.reddy@outlook.com>
2023-09-06 10:03:01 +0530

    made pages responsive and added routing for all navbar buttons

--- pages/hero.tsx
+++ pages/hero.tsx
@@ -5,23 +5,27 @@ import ContactUs from './contactUs';
 
 export default function Hero() {
     return (
-        <div className='bg-ternary-color shadow-lg shadow-zinc-300'>
-        <br />
-        <div>
-            <h1 className="text-6xl font-bold text-gray-800 ">Databracket.ai</h1>
-            <p className='font-bold text-2xl text-popular-light mb-4'>Uniting Data and AI Solutions.</p>
-        </div>
-        <div className="flex items-center justify-center">
-            <Image src={imgFile} alt="This is the main image" width={500} height={50}></Image>
-        </div>
-        <div className='flex justify-center items-center pb-3'>
-        <h3 className="text-center text-lg">
-            Databracket is committed at furnishing teams/individuals with the knowledge, resources, and strategies to  <br />
-            excel at leveraging the potential of <b>Data Engineering, DevOps and MLOps</b><br />
-            and transform innovative <b>AI</b> concepts/ideas into commercial products/services.
-        </h3>
-        </div>
-        <ContactUs/>
+        <div className="bg-ternary-color shadow-lg shadow-zinc-300">
+            <div className="p-4 lg:p-8 xl:p-16"> {/* Adjust padding for different screen sizes */}
+                <h1 className="text-4xl md:text-6xl font-bold text-gray-800">Databracket.ai</h1>
+                <p className="font-bold text-xl md:text-2xl text-popular-light mb-4">
+                Uniting Data and AI Solutions.
+                </p>
+            </div>
+            <div className="flex items-center justify-center">
+                <Image src={imgFile} alt="This is the main image" width={500} height={50} />
+            </div>
+            <div className="p-4 md:p-8 xl:p-16"> {/* Adjust padding for different screen sizes */}
+                <h3 className="text-center text-lg md:text-xl">
+                Databracket is committed at furnishing teams/individuals with the knowledge,
+                resources, and strategies to
+                <br />
+                excel at leveraging the potential of <b>Data Engineering, DevOps and MLOps</b>
+                <br />
+                and transform innovative <b>AI</b> concepts/ideas into commercial products/services.
+                </h3>
+            </div>
+            <ContactUs />
         </div>
         )
   }
