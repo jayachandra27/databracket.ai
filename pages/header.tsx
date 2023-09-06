bcf9ad9fba38b1b70441f7c36a3393884e4979b1
jayachandra27 <jayachandra.s.reddy@outlook.com>
2023-09-06 18:28:15 +0530

    renamed header buttons

--- pages/header.tsx
+++ pages/header.tsx
@@ -71,10 +71,10 @@ export default function Header() {
                     <Link href="/courses">Courses</Link>
                 </button>
                 <button className="bg-white hover:bg-yellow-400 text-black hover:text-black px-4 py-2 rounded-full">
-                    <Link href="/devops">Podcasts</Link>
+                    <Link href="/podcasts">Podcasts</Link>
                 </button>
                 <button className="bg-white hover:bg-yellow-400 text-black hover:text-black px-4 py-2 rounded-full">
-                    <Link href="/mlops">Projects</Link>
+                    <Link href="/projects">Projects</Link>
                 </button>
             </div>
             <div className={`md:flex md:space-x-4 ${isSmallScreen ? 'hidden' : 'mt-4'}`}>
